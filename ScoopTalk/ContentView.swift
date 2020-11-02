2bbe03bd93fbaf12945e44c49330cd93a564aa91
Harish <harishsg99@gmail.com>
2020-11-02 19:06:56 +0530

    Initial Commit

---
+++ ScoopTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScoopTalk
+//
+//  Created by Harish on 02/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScoopTalk/ScoopTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoopTalkApp.swift
+//  ScoopTalk
+//
+//  Created by Harish on 02/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ScoopTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
